5957ecc4addb0484b45d29b4a4c2e2767b1eab53
Tzeentche <ilyasugako87@gmail.com>
2019-12-11 13:44:55 +0300

    Small pack for approving or disapproving Fermi theorem

---
+++ Testing/src/Hello.java
@@ -0,0 +1,30 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        int a, b, c;
+
+        for (int n = 3; n < 11; n++) {
+
+            for (a = 100000; a < 1000000; a++) {
+
+                for (b = 100000; b < 1000000; b++) {
+
+                    for (c = 100000; c < 1000000; c++) {
+
+                        if (Math.pow(a, n) + Math.pow(b, n) == Math.pow(c, n)) {
+
+                            System.out.println("A * N= " + Math.pow(a, n) + "; B * N= " + Math.pow(b, n) + "; C * N= " + Math.pow(c, n));
+                            System.out.println("A= " + a + "; B= " + b + "; C= " + c);
+                            System.out.println("N= " + n);
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("Hello World");
+
+    }
+
+}
