968291cc1002fc7400a9df8184910c7a85c074da
Tzeentche <ilyasugako87@gmail.com>
2019-12-18 11:46:26 +0300

    Added interfaces for work with Hibernate, project's structure was rectored.

---
+++ Master/maven-demo/src/main/java/by/dpm/samples/App.java
@@ -0,0 +1,13 @@
+package by.dpm.samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
